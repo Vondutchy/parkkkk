0559f23a5624dd9dfafab34d2c57a421076b44a2
Von <22-05321@g.batstate-u.edu.ph>
2025-04-29 13:52:51 +0800

    First Commit

---
+++ app/src/main/java/com/example/parkingtangina/CreateAccountActivity.kt
@@ -0,0 +1,12 @@
+package com.example.parkeng
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.parkingtangina.R
+
+class CreateAccountActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account)
+    }
+}
