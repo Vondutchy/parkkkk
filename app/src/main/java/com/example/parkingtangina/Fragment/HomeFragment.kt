8ef294091701a51e50c354fd54c3c7060026b365
braderafsss <22-04238@g.batstate-u.edu.ph>
2025-05-02 22:42:27 +0800

    Fourth Commit

--- app/src/main/java/com/example/parkingtangina/Fragment/HomeFragment.kt
+++ app/src/main/java/com/example/parkingtangina/Fragment/HomeFragment.kt
@@ -5,10 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.models.SlideModel
 import com.example.parkingtangina.R
+import com.example.parkingtangina.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,9 +24,22 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+
+        val imageList = ArrayList<SlideModel>()
+        imageList.add(SlideModel(R.drawable.home_background, ScaleTypes.FIT))
+        imageList.add(SlideModel(R.drawable.home_background, ScaleTypes.FIT))
+        imageList.add(SlideModel(R.drawable.home_background, ScaleTypes.FIT))
+
+        val imageSlider = binding.imageSlider
+        imageSlider.setImageList(imageList)
+        imageSlider.setImageList(imageList, ScaleTypes.FIT)
+    }
     companion object {
 
     }
